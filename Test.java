39be967e4f72a9f9467974123a40c2efd2a13599
Siyuan Tian <tiansiyuan@gmail.com>
2017-08-01 21:42:18 +0800

    第一个

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] args){
+                test();
+        }
+        public static void test(){
+        // ...
+                System.out.println("第0个数字:" + 0);
+        }
+}
