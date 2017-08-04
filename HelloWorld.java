21ccd91cd1d74bbfe68fa0f492c7226d2bd36e27
Siyuan Tian <tiansiyuan@gmail.com>
2017-08-04 15:46:30 +0800

    排序和输出前三放进一个方法

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,26 +4,27 @@ public class HelloWorld {
     //完成 main 方法
     public static void main(String[] args) {
         int scores[]= {89,-23,64,91,119,52,73};
-        //int sorted[];
 
-        System.out.println("数组："); // +scores[]);
+        System.out.println("数组："); 
         for ( int i = 0; i < scores.length; i++ ) {
             System.out.print("scores[" + i + "] = " + scores[i]+", ");
         }
-        // System.out.println();
         System.out.println("数组长度 = " + scores.length);
-        // System.out.println("数组越界值：" + scores[scores.length]);
-        Arrays.sort(scores);
+        top3(scores);
+    }
+
+    public static void top3(int[] scrs) {
+        Arrays.sort(scrs);
         int j = 0;
-        for ( int i = scores.length - 1; i >= 0; i-- ) {
-            if ((scores[i] > 100) || (scores[i] < 0)) {
+        for ( int i = scrs.length - 1; i >= 0; i-- ) {
+            if ((scrs[i] > 100) || (scrs[i] < 0)) {
                 continue;
             }
             if (j == 3){
                 break;
             }
             j++;
-            System.out.println(scores[i]);
+            System.out.println(scrs[i]);
         }
     }
 }
